8fdd09809d89143517989e43c43e63f0ca70618f
Ola Aarbogen <ola@smartmedia.no>
2014-02-06 12:08:08 +0100

    corrected the respond script

--- footer.php
+++ footer.php
@@ -57,16 +57,16 @@
 
             if(typeof src !== 'undefined' && src.indexOf(find) != -1) {
                 var url = parseUri(src),
-                    main = parseUri('<?php bloginfo('wpurl'); ?>'),
+                    main = parseUri('<?php //bloginfo('wpurl'); ?>'),
                     replace = src.replace(url.host, main.host);
 
                 console.log(url);
                 $(this).attr('src', replace);
 
                 var link1 = $('#respond-proxy').attr('href');
-                var link1ok = link1.replace(url.host, main.host);
+                var link1ok = link1.replace(main.host, url.host);
                 var link2 = $('#respond-redirect').attr('href');
-                var link2ok = link2.replace(main.host, url.host);
+                var link2ok = link2.replace(url.host, main.host);
 
                 $('#respond-proxy').attr('href', link1ok);
                 $('#respond-redirect').attr('href', link2ok);
